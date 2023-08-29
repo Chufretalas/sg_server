ceffe6a88c7a268de3585319c11d9a5b3012c650
Chufretalas <marcobene.contact@gmail.com>
2023-08-29 12:23:00 -0300

    added an endpoint to check the connection to the LD

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,6 +1,12 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { SG_PASSWORD } from '$env/static/private'
 
-export const GET: RequestHandler = () => {
-    return new Response("all seens to be working fine", { status: 200 });
+export const GET: RequestHandler = async ({ request }) => {
+    const password = request.headers.get("authorization")?.split(" ")[1]
+    console.log(password, SG_PASSWORD)
+    if (!password || password !== SG_PASSWORD) {
+        throw error(401, "invalid password")
+    }
+    return new Response("ok", { status: 200 });
 };
